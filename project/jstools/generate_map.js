f8e86f9f177a521e954b44996149b0bd931e6537
Rapha <rphlo@users.noreply.github.com>
2025-11-17 03:06:17 +0200

    Update generate_map.js

--- project/jstools/generate_map.js
+++ project/jstools/generate_map.js
@@ -2,37 +2,23 @@ const fs = require("fs");
 const { loadImage } = require("canvas");
 const { drawRoute } = require("./drawHelpers");
 
-const [imgFile, routeFile, cornersJSON, type, tz] = process.argv.slice(2);
-
-const routeJSON = fs.readFileSync(routeFile, { encoding: "utf8", flag: "r" });
-const routeRaw = JSON.parse(routeJSON);
-const route = routeRaw.map((p) => {
-  return { time: p.time * 1e3, latLon: p.latlon };
-});
-const cornersRaw = JSON.parse(cornersJSON);
-const corners = {
-  top_left: {
-    lat: parseFloat(cornersRaw.top_left[0]),
-    lon: parseFloat(cornersRaw.top_left[1]),
-  },
-  top_right: {
-    lat: parseFloat(cornersRaw.top_right[0]),
-    lon: parseFloat(cornersRaw.top_right[1]),
-  },
-  bottom_right: {
-    lat: parseFloat(cornersRaw.bottom_right[0]),
-    lon: parseFloat(cornersRaw.bottom_right[1]),
-  },
-  bottom_left: {
-    lat: parseFloat(cornersRaw.bottom_left[0]),
-    lon: parseFloat(cornersRaw.bottom_left[1]),
-  },
-};
-const showHeader = type.includes("h");
-const showRoute = type.includes("r");
-
-(async (imageFile, route, corners, showHeader, showRoute, timezone) => {
+void async function (sysArgs) {
+  const [imgFile, routeFile, cornersCoords, outputType, timezone] = sysArgs;
   const img = await loadImage(imageFile);
+  const cornersRaw = cornersCoords.split(",");
+  const corners = {
+    top_left: {lat: parseFloat(cornersRaw[0]), lon: parseFloat(cornersRaw[1])},
+    top_right: {lat: parseFloat(cornersRaw[2]), lon: parseFloat(cornersRaw[3])},
+    bottom_right: {lat: parseFloat(cornersRaw[4]), lon: parseFloat(cornersRaw[5])},
+    bottom_left: {lat: parseFloat(cornersRaw[6]), lon: parseFloat(cornersRaw[7])},
+  };
+  const showHeader = outputType.includes("h");
+  const showRoute = outputType.includes("r");
+  const routeJSON = fs.readFileSync(routeFile, { encoding: "utf8", flag: "r" });
+  const routeRaw = JSON.parse(routeJSON);
+  const route = routeRaw.map((p) => {
+    return { time: p.time * 1e3, latLon: p.latlon };
+  });
   const canvas = await drawRoute(
     img,
     corners,
@@ -41,5 +27,7 @@ const showRoute = type.includes("r");
     showRoute,
     timezone
   );
-  console.log(canvas.toDataURL("image/jpeg", { quality: 0.8 }));
-})(imgFile, route, corners, showHeader, showRoute, tz);
+  const dataURI = canvas.toDataURL("image/jpeg", { quality: 0.9 });
+  process.stdout.write(dataURI);
+  process.exit(0);
+}(process.argv.slice(2));
