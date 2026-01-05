8dfecb0df56605a3e56735794aa710fb0e2b6264
Raphael Stefanini <raphael@rphlo.com>
2026-01-05 09:27:17 +0200

    2026

---
+++ project/frontend/src/components/LogOut.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import useGlobalState from "../utils/useGlobalState";
+
+
+const LogOut = () => {
+  const globalState = useGlobalState();
+
+  const onLogout = async () => {
+    globalState.setUser({});
+  };
+
+  return (
+    <>
+    <h3><i className="fas fa-power-off"></i> Logout</h3>
+    <button type="button" onClick={onLogout} className="btn btn-danger">
+      <i className="fas fa-power-off"></i> Logout
+    </button>
+    </>
+  )
+};
+
+
+export default LogOut;
