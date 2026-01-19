3411f451acf9538c3ad9b6a3577ab7b34738712b
Raphael Stefanini <raphael@rphlo.com>
2026-01-19 09:45:06 +0200

    logot

---
+++ project/frontend/src/utils/useUser.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import useGlobalState from "../utils/useGlobalState";
+
+const useUser = () => {
+    const globalState = useGlobalState();
+    const { api_token } = globalState.user;
+
+    const [userData, setUserData] = React.useState(null);
+
+    React.useEffect(() => {
+    if (api_token) {
+        (async () => {
+            const res = await fetch(
+                import.meta.env.VITE_API_URL + "/v1/auth/user/",
+                {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Token " + api_token,
+                },
+                }
+            );
+            if (res.ok) {
+                const data = await res.json();
+                setUserData(data);
+                return
+            } else {
+                globalState.setUser(null);
+            }
+        })();
+    };
+    setUserData(null);
+    }, [api_token]);
+
+    return userData;
+}
+
+export default useUser;
